0bad9070e39dc73931ccf27ab91b46a2a443b405
Death <21cookej@roundwoodpark.co.uk>
2026-03-20 11:56:09 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-// sw.js
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      if (!response.headers.get("content-type")?.includes("text/html")) {
-        return response;
-      }
-      return response.text().then((html) => {
-        const injected = html.replace(
-          "</body>",
-          `<script src="/injector.js"><\/script></body>`
-        );
-        return new Response(injected, {
-          headers: { "content-type": "text/html" },
-        });
-      });
-    })
-  );
-});
+// // sw.js
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     fetch(event.request).then((response) => {
+//       if (!response.headers.get("content-type")?.includes("text/html")) {
+//         return response;
+//       }
+//       return response.text().then((html) => {
+//         const injected = html.replace(
+//           "</body>",
+//           `<script src="/injector.js"><\/script></body>`
+//         );
+//         return new Response(injected, {
+//           headers: { "content-type": "text/html" },
+//         });
+//       });
+//     })
+//   );
+// });
